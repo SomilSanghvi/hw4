b0269d35252e80546f52da901de1396b473c5b88
SomilSanghvi <somil.sanghvi@kellogg.northwestern.edu>
2026-03-17 10:06:55 +0000

    bug fix attempt 3

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -14,9 +14,10 @@ class EntriesController < ApplicationController
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
     @entry["user_id"] = @current_user["id"]
-    @entry.uploaded_image.attach(params["uploaded_image"])
+    if params["uploaded_image"].present?
+      @entry.uploaded_image.attach(params["uploaded_image"])
+    end
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
   end
-
 end
